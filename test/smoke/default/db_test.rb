90a4b664b3a5c2d769b4aafd1fa1a77fb51255fa
Nghiem Ba Hieu <hi3unb@gmail.com>
2017-07-28 16:45:07 +0700

    Update InSpec tests for db recipe

--- test/smoke/default/db_test.rb
+++ test/smoke/default/db_test.rb
@@ -5,8 +5,13 @@
 # The Inspec reference, with examples and extensive documentation, can be
 # found at http://inspec.io/docs/reference/resources/
 
-describe package('mysql-server') do
+describe package('mysql-server-5.5') do
   it { should be_installed }
+end
+
+describe service('mysql-default') do
+  it { should be_enabled }
+  it { should be_running }
 end
 
 describe port(3306) do
