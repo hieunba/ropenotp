958ea5bcd17151175c155861559eb613f564b139
Nghiem Ba Hieu <hi3unb@gmail.com>
2017-07-28 16:34:22 +0700

    Provision mysql 5.5 with community cookbook

--- recipes/db.rb
+++ recipes/db.rb
@@ -3,4 +3,16 @@
 # Recipe:: db
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
-package 'mysql-server'
+# Configure the MySQL client
+mysql_client 'default' do
+  version '5.5'
+  action :create
+end
+
+# Configure the MySQL service
+mysql_service 'default' do
+  port '3306'
+  version '5.5'
+  initial_root_password 'change me'
+  action [:create, :start]
+end
