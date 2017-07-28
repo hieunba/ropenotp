0ab58b71182776cbfde549a060d4df7c0557247e
Nghiem Ba Hieu <hi3unb@gmail.com>
2017-07-28 17:57:22 +0700

    Add more InSpec for db recipe

--- test/smoke/default/db_test.rb
+++ test/smoke/default/db_test.rb
@@ -14,6 +14,16 @@ describe service('mysql-default') do
   it { should be_running }
 end
 
+describe mysql_conf('/etc/mysql-default/my.cnf').params('mysqld') do
+  its('port') { should eq '3306' }
+  its('socket') { should eq '/run/mysql-default/mysqld.sock' }
+end
+
 describe port(3306) do
   it { should be_listening }
+  its('protocols') { should include('tcp') }
+end
+
+describe command("mysql -h 127.0.0.1 -uroot -pchange\\\ me -s -e 'show databases;'") do
+  its('stdout') { should match (/mysql/) }
 end
